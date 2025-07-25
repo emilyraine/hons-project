15417eee4c7c2051cdb9c8f16081f3095a0e1c34
Emily Raine <ememraine@gmail.com>
2025-07-25 18:57:56 +0200

    Fixed an error with the calculation of total steps.

--- environment/blockObject.py
+++ environment/blockObject.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from pyroborobo import Pyroborobo, SquareObject
 import util.globals as globals
 
@@ -8,7 +9,7 @@ class BlockObject(SquareObject):
     simulation_lifetime = globals.config.get("pSimulationLifetime", "int")
     population_size = globals.config.get("pPopulationSize", "int")
     evaluation_trials = globals.config.get("pEvaluationTrials", "int")
-    total_steps_per_sim_square = (population_size/4)*simulation_lifetime*evaluation_trials
+    total_steps_per_sim_square = (population_size/multiprocessing.cpu_count())*simulation_lifetime*evaluation_trials
     self.one_quarter = 0.25 * total_steps_per_sim_square
     self.half = 0.5 * total_steps_per_sim_square
     self.three_quarters = 0.75 * total_steps_per_sim_square

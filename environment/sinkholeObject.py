2ac838f6de84d445326a18ef3acc2006df72f19c
Emily Raine <ememraine@gmail.com>
2025-07-28 07:46:37 +0200

    Updated sinkhole code such that robots that are located in the area where a sinkhole is about to appear are moved.

--- environment/sinkholeObject.py
+++ environment/sinkholeObject.py
@@ -1,5 +1,7 @@
+import multiprocessing
 from pyroborobo import Pyroborobo, CircleObject
 import util.globals as globals
+import util.calculate as calculate
 
 # Represents a sinkhole used in the Medium Level
 class SinkholeObject(CircleObject):
@@ -8,7 +10,8 @@ class SinkholeObject(CircleObject):
     simulation_lifetime = globals.config.get("pSimulationLifetime", "int")
     population_size = globals.config.get("pPopulationSize", "int")
     evaluation_trials = globals.config.get("pEvaluationTrials", "int")
-    total_steps_per_sim_square = (population_size/4)*simulation_lifetime*evaluation_trials
+    # Calculate total steps for each simulation window, adjusting for parallelism (CPU count)
+    total_steps_per_sim_square = (population_size/multiprocessing.cpu_count())*simulation_lifetime*evaluation_trials
     self.one_quarter = 0.25 * total_steps_per_sim_square
     self.half = 0.5 * total_steps_per_sim_square
     self.three_quarters = 0.75 * total_steps_per_sim_square
@@ -31,10 +34,9 @@ class SinkholeObject(CircleObject):
         inner_controller = base_controller.controller  # Dog or Sheep controller
         robot_x = inner_controller.agent.absolute_position[0]
         robot_y = inner_controller.agent.absolute_position[1]
-        distance_x = robot_x - self.x
-        distance_y = robot_y - self.y
-        if ((distance_x * distance_x + distance_y * distance_y) <= (self.radius * self.radius)):
-          self.is_walked(robot_id)
+        if (calculate.distance_between_points(inner_controller.agent.absolute_position, (self.x, self.y)) <= self.radius + 8):
+          # Move robots located in the area where a sinkhole is about to appear
+          inner_controller.agent.set_position(robot_x, (robot_y - 120))
 
   def step(self):
       current_time = Pyroborobo.get().iterations
